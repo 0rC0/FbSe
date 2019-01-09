03f8cd5e29e83f6196a00062ddcfa089a50ff241
0rC0 <orco@paranoici.org>
2019-01-10 00:51:17 +0100

    ff profile

--- FbBot.py
+++ FbBot.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.firefox.options import Options
-import getpass
 from time import sleep
 from dateutil import parser
 from datetime import datetime
@@ -10,11 +8,13 @@ import json
 '''
 Example JSON config file
 {
-"email": "email"
-"pw": "password"
+"email": "email",
+"pw": "password",
+"ff_profile_path": "path",
 "pages_to_visit": ["url1",
                    "url2",
-                   "url3"]
+                   "url3"],
+"data": "path file.json"
 }
 '''
 
@@ -24,12 +24,12 @@ class FbBot():
     def __init__(self, config_file):
         with open(config_file, 'r') as f:
             j = json.load(f)
-        options = Options()
-        options.set_preference("dom.webnotifications.enabled", False)  # Allow Notifications
-        self.b = webdriver.Firefox(firefox_options=options)
+        self.profile = j['ff_profile_path']
         self.email = j['email']
         self.pw = j['pw']
         self.to_visit = j['pages_to_visit']
+        self.data = j['data']
+        self.b = webdriver.Firefox(self.profile)
 
     def login(self):
         # ToDo: check if the login had success
@@ -51,8 +51,12 @@ class FbBot():
         settings_menu.click()
 
     def scroll_down(self, y, n=1):
+        # A post is 680 - 900/1000 (depends on the comments showed)
         for i in range(n):
             self.b.execute_script('window.scrollBy(0, {0});'.format(y))
+
+    def scroll_to_post(self, element):
+        self.b.execute_script("arguments[0].scrollIntoView(true);", element)
 
     def go_to_page(self, page):
         # ToDo: this has to become a: search page name, click on the 1st search result
@@ -64,4 +68,9 @@ class FbBot():
         if not posts:
             self.scroll_down()
         times = [p.get_attribute('title') for p in posts[0].find_elements_by_xpath("//*[@class='_5pcq']/abbr")]
-        return set([datetime.timestamp(parser.parse(i)) for i in times])
+        timestamps = set([datetime.timestamp(parser.parse(i)) for i in times])
+        return timestamps
+
+    def write_json(self, data):
+        with open(self.data, 'w') as f:
+            json.dump(data, f)
